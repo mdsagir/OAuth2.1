1a3fb590384e74cae77a589bf760508ba7f1aee0
sagiransari <tech.sagir@gmail.com>
2025-04-28 16:15:23 +0530

    fixes the sonar issue

--- order-service/src/main/java/org/orders/model/Order.java
+++ order-service/src/main/java/org/orders/model/Order.java
@@ -14,14 +14,15 @@ public record Order(
         Instant lastModifiedDate,
         Integer version
 ) {
+    private static final Random RANDOM = new Random();
+
     public static Order of(String bookIsbn, String bookName, Double bookPrice, Integer quantity, OrderStatus status) {
         return new Order(generate(), bookIsbn, bookName, bookPrice, quantity, status, null, null, 0);
     }
 
     public static long generate() {
-        final Random random = new Random();
         long currentTimeMillis = System.currentTimeMillis();
-        int randomPart = random.nextInt(999); // random 0-998
+        int randomPart = RANDOM.nextInt(999); // random 0-998
         return (currentTimeMillis % Integer.MAX_VALUE) + randomPart;
     }
 }
